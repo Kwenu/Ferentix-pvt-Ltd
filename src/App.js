964a7eb5392c8afa0ba4f6c8aba4044e0cdbe512
Kwenu <128037548+Kwenu@users.noreply.github.com>
2024-09-29 18:51:01 +0530

    Error fixing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./Navbar";
 import LandingPage from "./LandingPage";
 import WelcomeSection from "./WelcomeSection";
