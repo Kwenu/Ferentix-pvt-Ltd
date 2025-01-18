1b49ed15cdcb90b07d899c62462490bd5a349b54
Kwenu <128037548+Kwenu@users.noreply.github.com>
2025-01-18 23:50:17 +0530

    responsive part 1

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,20 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Navbar.css';
 import Logo from '../src/logo.jpg';
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <nav className="navbar">
       <div className="navbar-logo">
         <img src={Logo} alt="logo" />
       </div>
-      <ul className="navbar-links">
+      <div className="hamburger" onClick={toggleMenu}>
+        <span className="bar"></span>
+        <span className="bar"></span>
+        <span className="bar"></span>
+      </div>
+      <ul className={`navbar-links ${isMenuOpen ? 'active' : ''}`}>
         <li><a href="/">Home</a></li>
         <li><a href="/about">About Us</a></li>
         <li><a href="/solar">Solar</a></li>
         <li><a href="/construction">Construction</a></li>
         <li><a href="/projects">Projects</a></li>
-        <li className='contact-btn'><a href="/contact">Contact Us</a></li>
+        <li className="contact-btn"><a href="/contact">Contact Us</a></li>
       </ul>
     </nav>
   );
