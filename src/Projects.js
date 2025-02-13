e28660b65cb52fd4f32c5b96a8d9dbd94f6b2193
Kwenu <128037548+Kwenu@users.noreply.github.com>
2025-02-13 11:35:23 +0530

    Final final

--- src/Projects.js
+++ src/Projects.js
@@ -1,5 +1,38 @@
 import React from "react";
 import "./Projects.css";
+import Img1 from "../src/projects/1.jpg";
+import Img2 from "../src/projects/2.jpg";
+import Img3 from "../src/projects/3.jpg";
+import Img4 from "../src/projects/4.jpg";
+import Img5 from "../src/projects/5.jpg";
+import Img6 from "../src/projects/6.jpg";
+import Img7 from "../src/projects/7.jpg";
+import Img8 from "../src/projects/8.jpg";
+import Img9 from "../src/projects/9.jpg";
+import Img10 from "../src/projects/10.jpg";
+import Img11 from "../src/projects/11.jpg";
+import Img12 from "../src/projects/12.jpg";
+import Img13 from "../src/projects/13.jpg";
+import Img14 from "../src/projects/14.jpg";
+import Img15 from "../src/projects/15.jpg";
+import Img17 from "../src/projects/17.jpg";
+import Img18 from "../src/projects/18.jpg";
+import Img19 from "../src/projects/19.jpg";
+import Img20 from "../src/projects/20.jpg";
+import Img21 from "../src/projects/21.jpg";
+import Img22 from "../src/projects/22.jpg";
+import Img23 from "../src/projects/23.jpg";
+import Img24 from "../src/projects/24.jpg";
+import Img25 from "../src/projects/25.jpg";
+import Img26 from "../src/projects/27.jpg";
+import Img27 from "../src/projects/28.jpg";
+import Img28 from "../src/projects/29.jpg";
+import Img29 from "../src/projects/30.jpg";
+import Img30 from "../src/projects/31.jpg";
+import Img31 from "../src/projects/32.jpg";
+import Img32 from "../src/projects/33.jpg";
+import Img33 from "../src/projects/34.jpg";
+import Img34 from "../src/projects/35.jpg";
 
 const Projects = () => {
   return (
@@ -18,6 +51,46 @@ const Projects = () => {
         costs, and promote sustainability. From on-grid to off-grid and 
         hybrid solar systems, Ferentix ensures every project is executed 
         with precision and long-term reliability. </p>
+      </div>
+      <div className="projects-body">
+        <div className="body-title">
+          <h2>Solar Projects</h2>
+        </div>
+        <div className="body-gallery">
+          <img src={Img1} alt="" />
+          <img src={Img2} alt="" />
+          <img src={Img3} alt="" />
+          <img src={Img4} alt="" />
+          <img src={Img5} alt="" />
+          <img src={Img6} alt="" />
+          <img src={Img7} alt="" />
+          <img src={Img8} alt="" />
+          <img src={Img9} alt="" />
+          <img src={Img10} alt="" />
+          <img src={Img11} alt="" />
+          <img src={Img12} alt="" />
+          <img src={Img13} alt="" />
+          <img src={Img14} alt="" />
+          <img src={Img15} alt="" />
+          <img src={Img17} alt="" />
+          <img src={Img18} alt="" />
+          <img src={Img19} alt="" />
+          <img src={Img20} alt="" />
+          <img src={Img21} alt="" />
+          <img src={Img22} alt="" />
+          <img src={Img23} alt="" />
+          <img src={Img24} alt="" />
+          <img src={Img25} alt="" />
+          <img src={Img26} alt="" />
+          <img src={Img27} alt="" />
+          <img src={Img28} alt="" />
+          <img src={Img29} alt="" />
+          <img src={Img30} alt="" />
+          <img src={Img31} alt="" />
+          <img src={Img32} alt="" />
+          <img src={Img33} alt="" />
+          <img src={Img34} alt="" />
+        </div>
       </div>
     </div>
   );
